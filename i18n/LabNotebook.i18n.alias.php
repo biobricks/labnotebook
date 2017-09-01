4932ed6feb154cf198a3786e39c8bb2902c63a29
yar <yardenack@gmail.com>
2017-08-31 20:00:33 -0700

    labnotebook: alias file for special page names

---
+++ i18n/LabNotebook.i18n.alias.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Aliases for LabNotebook
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author Yardena Cohen
+ */
+$specialPageAliases['en'] = array(
+    'LabNotebook' => array( 'LabNotebook', 'Lab Notebook' ),
+    'NewNotebook' => array( 'NewNotebook', 'New Notebook', 'newnotebook', 'new notebook' ),
+);
