92eb11a3d2c72e50a8b1b0f2f8d74e8417b4b278
yar <yardenack@gmail.com>
2017-08-31 20:15:09 -0700

    labnotebook: trying to get resourceloader to work

--- LabNotebook_body.php
+++ LabNotebook_body.php
@@ -22,6 +22,7 @@ class NewNotebook extends SpecialPage {
         $output = $this->getOutput();
         $this->setHeaders();
         $param = $request->getText( 'param' ); # Get request data from, e.g.
+        $output->addModules( 'ext.LabNotebook.oneclick' );
         $output->addHTML(file_get_contents(__DIR__.'/includes/create.html'));
         $output->addWikiText('[[category:OWWLabNotebookV1]]');
     }
