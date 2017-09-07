5f0718d9c0638a4bbfcff9af38f9f6af19e73d19
yar <yardenack@gmail.com>
2017-09-06 17:26:21 -0700

    labnotebook: npus
    a very simple api module to figure out what works & what went wrong
    
    looks like the problem is trying to overload construct()...

---
+++ includes/npus.php
@@ -0,0 +1,26 @@
+<?php
+class npusdo extends ApiBase{
+    public function getAllowedParams() {
+        return array(
+            'n' => array(
+                ApiBase::PARAM_TYPE => 'integer',
+                ApiBase::PARAM_REQUIRED => true,
+                ApiBase::PARAM_DFLT => '5',
+            ),
+            'name' => array(
+                ApiBase::PARAM_TYPE => 'string',
+                ApiBase::PARAM_REQUIRED => true,
+                ApiBase::PARAM_DFLT => 'Bob',
+            ),
+        );
+    }
+	public function isWriteMode() {
+		return true;
+	}
+	public function execute() {
+        $params = $this->extractRequestParams();
+		$this->getResult()->addValue( null, 'npus', "the npus ".$params['name']." has ".$params['n']." tentacles");
+        return $this->getResult();
+	}
+}
+?>
