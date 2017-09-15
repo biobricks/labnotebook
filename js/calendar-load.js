127c676e7e6fc51e464cc053f84d85cd350da706
yar <yardenack@gmail.com>
2017-09-15 14:08:23 -0700

    labnotebook: make year view work
    now we can handle multiple calendars on one page, with or without a <p>

--- js/calendar-load.js
+++ js/calendar-load.js
@@ -6,48 +6,74 @@ function htmlDecode(input){
 
 function lnCalendarLoad() {
 
+    // this will be the list of DOM elements containing proto-calendar children
+    // i.e. the hidden divs written by LNCalendar.php
+    var cals = []
+
     // LNCalendar.php wrote some divs inside the <p> tags.
     // Bring them out so they can actually be elements
+    // turn <td><p>&lt;!-- into <td><!--
     var ps = document.getElementsByTagName("p")
     var n = 0
     for (var p in ps) {
         if (ps[n] && ps[n].innerHTML && /^&lt;!-- sibboleth/.test(ps[n].innerHTML)) {
             var heldhtml = ps[n].innerHTML
-            var td = ps[n].parentElement
-            td.innerHTML = ''
-            td.innerHTML = htmlDecode(heldhtml)
-            break
+            cals.push({container:ps[n].parentElement})
+            var ptd = ps[n].parentElement
+            ptd.innerHTML = ''
+            ptd.innerHTML = htmlDecode(heldhtml)
         }
         n++
     }
 
-    // use those hidden divs to assign vars
-    var opts = []
-    for (var i=0; i < td.children.length; i++) {
-        if ((td.children[i].tagName.toLowerCase() == 'div') && (td.children[i].id == 'lncal1')) {
-            for (var j=0; j < td.children[i].children.length; j++) {
-                var hiddendiv = td.children[i].children[j]
-                if (hiddendiv.tagName.toLowerCase() == 'div') {
-                    opts[hiddendiv.id] = hiddendiv.innerHTML
+    // the year page has multiple calendars
+    // not inside <p> but still displayed literally, need to be decoded
+    // turn <td>&lt;!-- into <td><!--
+    var tds = document.getElementsByTagName("td")
+    var n = 0
+    for (var td in tds) {
+        if (tds[n] && tds[n].innerHTML && /^&lt;!-- sibboleth/.test(tds[n].innerHTML)) {
+            var heldhtml = tds[n].innerHTML
+            cals.push({container:tds[n]})
+            var ptd = tds[n]
+            tds[n].innerHTML = ''
+            tds[n].innerHTML = htmlDecode(heldhtml)
+        }
+        n++
+    }
+
+    // for each calendar on the page
+    for (var k=0; k < cals.length; k++) {
+        var c = cals[k].container
+
+        // use those hidden divs to assign vars
+        cals[k].opts = []
+        for (var i=0; i < c.children.length; i++) {
+            if ((c.children[i].tagName.toLowerCase() == 'div') && (c.children[i].id.match(/^(lncal1|y[0-9]+)$/))) {
+                for (var j=0; j < c.children[i].children.length; j++) {
+                    var hiddendiv = c.children[i].children[j]
+                    if (hiddendiv.tagName.toLowerCase() == 'div') {
+                        cals[k].opts[hiddendiv.id] = hiddendiv.innerHTML
+                    }
                 }
             }
         }
-    }
-    var cal = new CalendarPopup(opts['id'])
-    cal.setDateFormat(opts['fmt'])
-    cal.setCssPrefix(opts['css'])
-    if (opts['month']) cal.setSingleMonth(opts['month'])
-    if (opts['year']) cal.setYear(opts['year'])
-    cal.setReadOnly(opts['readonly'])
-    cal.setTodayText('')
-    cal.setUrlPrefix('/wiki/Special:Redir/'+opts['page'])
-    var dates = opts['dtext'].split(',')
-    for (var i=0; i<dates.length; i++) {
-        if (dates[i]) cal.addFilledDates(dates[i])
-    }
+        cals[k].popup = new CalendarPopup(cals[k].opts['id'])
+        cals[k].popup.setDateFormat(cals[k].opts['fmt'])
+        cals[k].popup.setCssPrefix(cals[k].opts['css'])
+        if (cals[k].opts['month']) cals[k].popup.setSingleMonth(cals[k].opts['month'])
+        if (cals[k].opts['year']) cals[k].popup.setYear(cals[k].opts['year'])
+        cals[k].popup.setReadOnly(cals[k].opts['readonly'])
+        cals[k].popup.setTodayText('')
+        cals[k].popup.setUrlPrefix('/wiki/Special:Redir/'+cals[k].opts['page'])
+        var dates = cals[k].opts['dtext'].split(',')
+        for (var i=0; i<dates.length; i++) {
+            if (dates[i]) cals[k].popup.addFilledDates(dates[i])
+        }
 
-    // now show calendar
-    cal.showCalendar(opts['id'])
+        // now show calendar
+        cals[k].popup.showCalendar(cals[k].opts['id'])
+    }
 }
 
 function CalendarPageConfirmCreate(date, url) {
@@ -59,6 +85,7 @@ function CalendarPageConfirmCreate(date, url) {
 window.htmlDecode = htmlDecode
 window.lnCalendarLoad = lnCalendarLoad
 window.CalendarPageConfirmCreate = CalendarPageConfirmCreate
+window.CP_refreshCalendar = CP_refreshCalendar
 
 jQuery(document).ready(function($) {
     lnCalendarLoad()
